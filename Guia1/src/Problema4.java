f07ca3f85c722fa7e70df14f7434c307085224f6
lespindo8284 <lespindo8284@universidadean.edu.co>
2017-07-20 21:48:32 -0500

    Add files via upload

--- Guia1/src/Problema4.java
+++ Guia1/src/Problema4.java
@@ -8,17 +8,24 @@ public class Problema4 {
 		System.out.print("Introduzca la cantidad de casos de prueba: ");
 		int cantidad = reader.nextInt();
 		int arreglo[]=new int[cantidad];
-		int j=1;
+		int j=0;
+		int contador=0;
 		for(int i=0;i<cantidad;i++) {
 			System.out.print("Valor "+j+++" : ");
 			arreglo[i]=reader.nextInt();	
 		}
-		
-		j=1;
 		System.out.println("Resultado");
 		for(int i=0;i<cantidad;i++) {
-			System.out.println("Valor "+j+++" : "+Integer.bitCount(arreglo[i]));	
+			if(arreglo[i]>=1&&arreglo[i]<=10000000) {
+				for (j=5; arreglo[i]/j>=1; j *= 5) {
+		            contador += arreglo[i]/j;
+				}
+			}
+			else {
+				System.out.println("NUMERO ERRONEO");
+			}
+			System.out.println("Valor para "+arreglo[i]+" : "+contador);	
+			contador=0;
 		}
-		
 	}
 }
